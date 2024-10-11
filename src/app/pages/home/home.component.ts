8d25c5ba3be8e9ad01c1aa39a60c663c872bc598
MarcoCipolletta <marcocipolletta1998@gmail.com>
2024-10-11 13:46:56 +0200

    Done hompage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { icar } from '../../interfaces/icar';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  cars: icar[] = [];
+  twoRndmCar: icar[] = [];
 
+  ngOnInit() {
+    fetch('db.json')
+      .then((res) => <Promise<icar[]>>res.json())
+      .then((arr) => {
+        const arrayIndexRndmCars: number[] = [];
+        while (this.twoRndmCar.length < 2) {
+          let randomCarIndex = Math.floor(Math.random() * arr.length);
+
+          if (!arrayIndexRndmCars.includes(randomCarIndex)) {
+            this.twoRndmCar.push(arr[randomCarIndex]);
+            arrayIndexRndmCars.push(randomCarIndex);
+          }
+        }
+      });
+  }
 }
